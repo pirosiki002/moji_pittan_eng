a6fddb264c53e1f277b4d13186db74b6c99c9634
SI <>
2022-02-13 16:24:49 +0900

    sqliteへのアクセス処理（アクセス不可）

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-/* main */
+
+console.log("Javascript activate test");
+// /* main */
+// const sqlite3 = require('sqlite3');
+// ここからはザキさんに相談かな？
+var sqlite3 = require("sqlite3").verbose();
+// // 作成したDBに接続
+// const db = new sqlite3.Database('/db/ejdict.sqlite3');
+
+// db.all('SELECT * FROM items', (err, rows) =>{
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+//     rows.forEach((row) =>{
+//         // console.log('id:' +row.id + ' name:' + row.name);
+//         console.log('word:' +row.word + ' mean:' + row.mean + 'level:' + row.level);
+//     })
+// });
