abcb94ee8a7862fab9f10f8e21a2d5ab918e0ec1
SI <>
2022-02-15 15:04:08 +0900

    辞書の参照処理を追加

--- main.js
+++ main.js
@@ -5,15 +5,15 @@ console.log("Javascript activate test");
 // ここからはザキさんに相談かな？
 var sqlite3 = require("sqlite3").verbose();
 // // 作成したDBに接続
-// const db = new sqlite3.Database('/db/ejdict.sqlite3');
+const db = new sqlite3.Database('./db/ejdict.sqlite3');
 
-// db.all('SELECT * FROM items', (err, rows) =>{
-//     if (err) {
-//         console.log(err);
-//         return;
-//     }
-//     rows.forEach((row) =>{
-//         // console.log('id:' +row.id + ' name:' + row.name);
-//         console.log('word:' +row.word + ' mean:' + row.mean + 'level:' + row.level);
-//     })
-// });
+db.all('SELECT * FROM items', (err, rows) =>{
+    if (err) {
+        console.log(err);
+        return;
+    }
+    rows.forEach((row) =>{
+        // console.log('id:' +row.id + ' name:' + row.name);
+        console.log('word:' +row.word + ' mean:' + row.mean + 'level:' + row.level);
+    })
+});
